adc0939991017f323d3e317844e4f2931f95a8de
RCLEMENT404 <clement_raphael@orange.fr>
2023-01-05 16:35:56 +0100

    Allumage / Arret

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ class Robot():
     __states = ['shutdown', 'running']
 
     def __str__(self) -> str:
-        return "New robot %s created !"%self.name
-    def __init__(self, name,currentState):
+        return "New robot %s created and %s!"%(self.name,self.currentState)
+    def __init__(self, name):
         self.name = name
-        
+        self.currentState = self.__states[0]
+    def changeState(self):
+        self.currentState =  self.__states[1] if self.__states[0] else self.__states[0]
+
+
 r1 = Robot('R2D2');
+print(r1)
+r1.changeState()
 print(r1)
