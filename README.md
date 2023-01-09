5caf999d2d00bf828422e3239ec92154ddf78adc
RCLEMENT404 <clement_raphael@orange.fr>
2023-01-09 11:44:08 +0100

    Ex3

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
     - [Exigences](#exigences)
   - [EXO 2](#exo-2)
     - [Exigences](#exigences-1)
+  - [EXO 3](#exo-3)
+    - [Exigences](#exigences-2)
+      - [Aide](#aide)
 
 ## EXO 1
 
@@ -30,3 +33,19 @@
 * Un humain doit pouvoir digérer ce qu'il a mangé *pas très important, faire en dernier si vous avez le temps*
 * Un Cyborg doit être une combinaison d'un humain et d'un robot
 * Bonus : ajouter une méthode fun au Cyborg
+
+
+## EXO 3
+
+### Exigences
+
+* Mettre en avant un principe de classe abstraite
+* Mettre en avant un principe de polymorphisme
+* Mettre en avant un principe d'héritage multiple
+* Max 3 méthodes par classe (hors getter/setter)
+* Pas d'algorithmes complexes, juste des print ;)
+* En tant que client, je veux pouvoir jouer avec trois types/dérivés de robots différents
+
+#### Aide
+
+Sortez un bon vieux crayon pour schématiser vos dépendances d'héritages
--- cyborg.py
+++ cyborg.py
@@ -2,14 +2,41 @@ from random import randint
 from robot import Robot
 from human import Human
 
+def SecurisedAction(f):
+	
+    def wrapper(*args):
+	
+        obj = args[0]
+	
+        if obj.safety_enabled:
+	
+            print('Robot is not allowed to do any action... ', str(f))
+	
+        else:
+	
+            return f(*args)
+	
+    return wrapper
+
 class Cyborg(Robot, Human):   
     __nuke = False
+    __safety = False
+    @property
+    def safety_enabled(self):
+	
+        return self.__safety
+	
+    def changeSafety(self):
+        self.__safety = not(self.__safety)
+        print("Safety changed to "+ str(self.__safety))
     def __init__(self, name, sexe, nuke=False):   
         Robot.__init__(self, name)
         Human.__init__(self, sexe)
         self.__nuke = nuke
         self.name = name
         self.sexe = sexe
+
+    
     def __del__(self):
         if self.__nuke == True:
             print("AUTODESTRUCTION COMPLETE")
@@ -22,6 +49,7 @@ class Cyborg(Robot, Human):
     def status(self):
         print(Robot.__str__(self))
         print(Human.__str__(self))
+    @SecurisedAction
     def changePayloadStatus(self):
         self.__nuke = not(self.__nuke)
 # ---------------------------------------------
@@ -37,5 +65,8 @@ if __name__ =='__main__':
     cyborg.eat('banana')
     cyborg.eat(['coca', 'chips'])
     cyborg.digest()
+    cyborg.changeSafety()
+    cyborg.changePayloadStatus()
+    cyborg.changeSafety()
     cyborg.changePayloadStatus()
     del cyborg
